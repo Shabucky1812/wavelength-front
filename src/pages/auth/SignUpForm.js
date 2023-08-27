279d815e1c03a7531b922a6e99e75266d3a41ca2
Shabucky1812 <shaunbuck1812@gmail.com>
2023-08-27 14:02:38 +0000

    Add basic sign up form fields

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React from "react";
 // css link
 import styles from "../../styles/SignInUpForm.module.css";
 // react-bootstrap components
-import Container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const SignUpForm = () => {
   return (
     <div className={styles.Background}>
-        <Container className={styles.FormContainer}>
-            <h2 className={styles.Title}>Create an Account</h2>
-            form goes here
-        </Container>
-    </div>
-  )
-}
+      <Container className={styles.FormContainer}>
+        <h2 className={styles.Title}>Create an Account</h2>
+        <hr />
+        <Form className={styles.Form}>
+          <Form.Group controlId="username">
+            <Form.Label className={styles.Label}>Username:</Form.Label>
+            <Form.Control type="text" placeholder="Username" name="username" />
+          </Form.Group>
 
-export default SignUpForm
+          <Form.Group controlId="password1">
+            <Form.Label className={styles.Label}>Password:</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="password1"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="password2">
+            <Form.Label className={styles.Label}>Confirm password:</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm password"
+              name="password2"
+            />
+          </Form.Group>
+
+          <Button type="submit">Sign up</Button>
+        </Form>
+      </Container>
+    </div>
+  );
+};
+
+export default SignUpForm;
