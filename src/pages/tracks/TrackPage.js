e1b5edfa3c23c54c7a21e7adea29166658619cfb
Shabucky1812 <shaunbuck1812@gmail.com>
2023-09-02 09:17:36 +0000

    Retrieve track from id

--- src/pages/tracks/TrackPage.js
+++ src/pages/tracks/TrackPage.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
 const TrackPage = () => {
+  const { id } = useParams();
+  const [track, setTrack] = useState({results: []})
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{data: track}] = await Promise.all([
+          axiosReq.get(`/tracks/${id}`)
+        ])
+        setTrack({results: [track]})
+        console.log(track)
+      } catch(err) {
+        // console.log(err)
+      }
+    }
+
+    handleMount()
+  }, [id])
+
   return (
     <div>
-        <p>Track Component</p>
-        <p>Create/Edit Review Form</p>
-        <p>Review List</p>
+      <p>Track Component</p>
+      <p>Create/Edit Review Form</p>
+      <p>Review List</p>
     </div>
-  )
-}
+  );
+};
 
-export default TrackPage
+export default TrackPage;
