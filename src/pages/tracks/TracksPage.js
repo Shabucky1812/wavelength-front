d96a356ab47a1daa37e33e6004de24215de67281
Shabucky1812 <shaunbuck1812@gmail.com>
2023-09-05 17:30:54 +0000

    Add basic search bar

--- src/pages/tracks/TracksPage.js
+++ src/pages/tracks/TracksPage.js
@@ -5,46 +5,56 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 // custom components
 import Track from "./Track";
-import Asset from "../../components/Asset"
+import Asset from "../../components/Asset";
 
-const TracksPage = ({message, filter=""}) => {
-  const [tracks, setTracks] = useState({results: []})
-  const [hasLoaded, setHasLoaded] = useState(false)
-  const {pathname} = useLocation()
+const TracksPage = ({ message, filter = "" }) => {
+  const [tracks, setTracks] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const { pathname } = useLocation();
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const {data} = await axiosReq.get(`/tracks/?${filter}`)
-        setTracks(data)
-        setHasLoaded(true)
-      } catch(err) {
+        const { data } = await axiosReq.get(`/tracks/?${filter}search=${searchQuery}`);
+        setTracks(data);
+        setHasLoaded(true);
+      } catch (err) {
         // console.log(err)
       }
-    }
+    };
 
-    setHasLoaded(false)
-    fetchPosts()
-  }, [filter, pathname])
+    setHasLoaded(false);
+    fetchPosts();
+  }, [filter, pathname, searchQuery]);
 
   return (
     <>
       <Row>
         <Col xs={6} md={8}>
-          Search Bar
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <Form onSubmit={(event) => event.preventDefault()}>
+            <Form.Control
+              type="text"
+              placeholder="Search Tracks"
+              value={searchQuery}
+              onChange={(event) => setSearchQuery(event.target.value)}
+            />
+          </Form>
         </Col>
         <Col xs={6} md={4}>
           Filter
         </Col>
       </Row>
       <Row>
-        <Col xs={12} md={{span: 10, offset: 1}}>
+        <Col xs={12} md={{ span: 10, offset: 1 }}>
           {hasLoaded ? (
             <>
               {tracks.results.length ? (
-                tracks.results.map(track => (
+                tracks.results.map((track) => (
                   <Track key={track.id} {...track} />
                 ))
               ) : (
