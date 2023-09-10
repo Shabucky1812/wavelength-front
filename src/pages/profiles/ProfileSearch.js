5e441c14740722ec6bca252a2faf359828df6b46
Shabucky1812 <shaunbuck1812@gmail.com>
2023-09-10 09:05:38 +0000

    Retrieve profileData

--- src/pages/profiles/ProfileSearch.js
+++ src/pages/profiles/ProfileSearch.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { axiosReq } from '../../api/axiosDefaults'
+import { useCurrentUser } from '../../contexts/CurrentUserContext'
+// react-bootstrap components
+import Container from 'react-bootstrap/Container'
 
 const ProfileSearch = ({message}) => {
+  const [profileData, setProfileData] = useState({results: []})
+  const [searchQuery, setSearchQuery] = useState("")
+  const currentUser = useCurrentUser()
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const {data} = await axiosReq.get(`/profiles/?ordering=-followers_count&search=${searchQuery}`)
+        setProfileData(data)
+      } catch(err) {
+        // console.log(err)
+      }
+    }
+
+    handleMount()
+  }, [currentUser, searchQuery])
+
   return (
-    <div>
+    <Container>
       <p>Search bar</p>
-      <p>profile preview list</p>
+      {profileData.results.map(profile => <p key={profile.id}>{profile.owner}</p>)}
       {message}
-    </div>
+    </Container>
   )
 }
 
