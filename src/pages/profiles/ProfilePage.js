17ea5701847ab02782f35ddd70858e55af677d78
Shabucky1812 <shaunbuck1812@gmail.com>
2023-09-11 16:22:44 +0000

    Conditionally render follow buttons

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -6,38 +6,47 @@ import Asset from "../../components/Asset";
 // react-bootstrap components
 import Container from "react-bootstrap/Container";
 import { axiosReq } from "../../api/axiosDefaults";
+import { Button } from "react-bootstrap";
 
 const ProfilePage = () => {
-  const [profile, setProfile] = useState({results: []})
-  const [hasLoaded, setHasLoaded] = useState(false)
-  const currentUser = useCurrentUser()
-  const {id} = useParams()
+  const [profile, setProfile] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === profile?.owner;
+  const { id } = useParams();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [{data: profile}] = await Promise.all([
-          axiosReq.get(`/profiles/${id}`)
-        ])
-        setProfile(profile)
-        setHasLoaded(true)
-      } catch(err) {
+        const [{ data: profile }] = await Promise.all([
+          axiosReq.get(`/profiles/${id}`),
+        ]);
+        setProfile(profile);
+        setHasLoaded(true);
+      } catch (err) {
         // console.log(err)
       }
-    }
+    };
 
     fetchData();
-  }, [id])
+  }, [id]);
 
   return (
     <Container>
       {hasLoaded ? (
-        <>
+        <div>
           <p>profile header info</p>
           {profile?.owner}
           {profile?.tracks_count}
           <p>tracks shared</p>
-        </>
+          {currentUser &&
+            !is_owner &&
+            (profile.following_id ? (
+              <Button onClick={() => {}}>unfollow</Button>
+            ) : (
+              <Button onClick={() => {}}>follow</Button>
+            ))}
+        </div>
       ) : (
         <Asset spinner />
       )}
