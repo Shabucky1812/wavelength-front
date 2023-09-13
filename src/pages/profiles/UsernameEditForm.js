676be0e2bc2289a694bd14315c2107ca743135a9
Shabucky1812 <shaunbuck1812@gmail.com>
2023-09-13 17:56:37 +0000

    Add username edit form

--- src/pages/profiles/UsernameEditForm.js
+++ src/pages/profiles/UsernameEditForm.js
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefaults";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../../contexts/CurrentUserContext";
+// react-bootstrap components
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-const ProfilePictureEditForm = () => {
+const UsernameEditForm = () => {
+  const [username, setUsername] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+  const { id } = useParams();
+
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+
+  useEffect(() => {
+    if (currentUser?.profile_id?.toString() === id) {
+      setUsername(currentUser.username);
+    } else {
+      history.push("/");
+    }
+  }, [currentUser, history, id]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put("/dj-rest-auth/user/", {
+        username,
+      });
+      setCurrentUser((prevUser) => ({
+        ...prevUser,
+        username,
+      }));
+      history.goBack();
+    } catch (err) {
+      // console.log(err);
+      setErrors(err.response?.data);
+    }
+  };
+
   return (
-    <div>
-        edit username
-    </div>
-  )
-}
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label>Change username</Form.Label>
+        <Form.Control
+          placeholder="username"
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </Form.Group>
+      {errors?.username?.map((message, idx) => (
+        <Alert key={idx} variant="warning">
+          {message}
+        </Alert>
+      ))}
+      <Button onClick={() => history.goBack()}>Cancel</Button>
+      <Button type="submit">Save</Button>
+    </Form>
+  );
+};
 
-export default ProfilePictureEditForm
+export default UsernameEditForm;
