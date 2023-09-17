6fd574f2f4f6393aae3e09c4039e29063194a9cb
Shabucky1812 <shaunbuck1812@gmail.com>
2023-09-17 18:50:33 +0000

    Add comments + docstrings to utils + app css

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,12 @@
 import jwtDecode from "jwt-decode";
 import { axiosReq } from "../api/axiosDefaults";
 
+/**
+ * Determines if the resource has more content to be loaded and fetches it if so.
+ * 
+ * @param {dict} resource - dictionary containing info about the passed in resource
+ * @param {function} setResource - function to update resource
+ */
 export const fetchMoreData = async (resource, setResource) => {
   try {
     const { data } = await axiosReq.get(resource.next);
@@ -18,6 +24,15 @@ export const fetchMoreData = async (resource, setResource) => {
   }
 };
 
+/**
+ * updates info around a new following
+ * 
+ * @param {dict} profile - profile doing the following
+ * @param {dict} clickedProfile - profile being followed
+ * @param {integer} following_id - id that represents the following instance being created
+ * 
+ * @returns changes to be made (if any) to the profile info
+ */
 export const followHelper = (profile, clickedProfile, following_id) => {
   return profile.id === clickedProfile.id
     ? {
@@ -33,6 +48,14 @@ export const followHelper = (profile, clickedProfile, following_id) => {
     : profile;
 };
 
+/**
+ * updates info around an unfollowing
+ * 
+ * @param {dict} profile - the profile doing the unfollowing
+ * @param {dict} clickedProfile the profile being unfollowed
+ * 
+ * @returns changes to be made (if any) to the profile info
+ */
 export const unfollowHelper = (profile, clickedProfile) => {
   return profile.id === clickedProfile.id
     ? {
@@ -48,15 +71,28 @@ export const unfollowHelper = (profile, clickedProfile) => {
     : profile;
 };
 
+/**
+ * sets refresh token for a signed in user
+ * 
+ * @param {dict} data - data about the sign in taking place
+ */
 export const setTokenTimestamp = (data) => {
   const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp
   localStorage.setItem('refreshTokenTimestamp', refreshTokenTimestamp)
 }
 
+/**
+ * determines if a token should be refreshed
+ * 
+ * @returns boolean value based on whether the user is logged in
+ */
 export const shouldRefreshToken = () => {
   return !!localStorage.getItem('refreshTokenTimestamp')
 }
 
+/**
+ * removes refresh token
+ */
 export const removeTokenTimestamp = () => {
   localStorage.removeItem('refreshTokenTimestamp')
 }
